f64b56d489b63ea0372d40cfa94cc5bcc9643eff
WaterKat <waterkatgames@gmail.com>
2019-11-27 12:46:38 -0800

    AudioUpdate

---
+++ Assets/SoundTester.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class SoundTester : MonoBehaviour
+{
+    [SerializeField]
+    AudioMixer mixer;
+    [Range(0,1)]
+    public float volumeslider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mixer.SetFloat("MasterVolume", volumeslider);    
+    }
+}
