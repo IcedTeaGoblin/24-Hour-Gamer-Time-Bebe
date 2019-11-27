b6da2b5e01ff0343e7c0423af27473d315539c75
WaterKat <waterkatgames@gmail.com>
2019-11-27 12:47:08 -0800

    ParticleManager

---
+++ Assets/WaterKat/ParticleSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSpawner : MonoBehaviour
+{
+    public GameObject Leaves;
+    public GameObject Wood;
+
+    [ContextMenu("SpawnLeaves")]
+    public void SpawnLeaves()
+    {
+        GameObject newLeaves = Instantiate(Leaves);
+        newLeaves.transform.position = transform.position;
+        newLeaves.SetActive(true);
+        newLeaves.transform.parent = transform;
+        Destroy(newLeaves, 1);
+    }
+
+    [ContextMenu("SpawnWood")]
+    public void SpawnWood()
+    {
+        GameObject newLeaves = Instantiate(Wood);
+        newLeaves.transform.position = transform.position;
+        newLeaves.SetActive(true);
+        newLeaves.transform.parent = transform;
+        Destroy(newLeaves, 1);
+    }
+
+    public void Start()
+    {
+        SpawnLeaves();
+        SpawnWood();
+
+    }
+}
